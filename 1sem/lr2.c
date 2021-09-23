8844c8365547d314e39009e83ced6aff64393ec1
quazzaar <89843200+quazzaar@users.noreply.github.com>
2021-09-23 22:39:58 +0300

    Update lr2.c

--- 1sem/lr2.c
+++ 1sem/lr2.c
@@ -6,13 +6,19 @@ int main()
     double z1, z2, a;
     double pi=3.1415926;
     scanf("%lf", &a);
+
+    if (a==45 || a==135) {
+        printf("%s","Error, division by 0");
+        return 0;
+    }
+    
     a=a*pi/180;
-    
+
     z1=(cos(a)+sin(a))/(cos(a)-sin(a));
-    printf("%.2f\n", z1);
-    
-    z2=tan(2*a)+1/cos(2*a);
-    printf("%.2f\n", z1);
-    
+    z2=tan(a*2)+1/cos(a*2);
+
+    printf("%.3f\n", z1);
+    printf("%.3f\n", z2);
+
     return 0;
 }
