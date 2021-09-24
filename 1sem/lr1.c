db0768dc7f18f5b6a74d6eca1638cac8188ca04d
quazzaar <89843200+quazzaar@users.noreply.github.com>
2021-09-24 09:41:48 +0300

    Update lr1.c

--- 1sem/lr1.c
+++ 1sem/lr1.c
@@ -2,14 +2,14 @@
 
 int main()
 {
-    int number;
-    char str[80];
+    int number; 
+    char str[80]; // объявление переменных
 
-    scanf("%[^\n]s", str);
-    scanf("%d", &number);
+    scanf("%[^\n]s", str); 
+    scanf("%d", &number); //сканирование строки и целого числа с консоли
 
-    printf("%s\n", str);
-    printf("%d\n", number);
+    printf("%s\n", str); 
+    printf("%d\n", number); //вывод строки и числа в консоль
 
     return 0;
 }
