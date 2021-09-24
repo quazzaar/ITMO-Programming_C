f9d5605719b79c09e9e58d4b0beb1585d2b9ad70
quazzaar <89843200+quazzaar@users.noreply.github.com>
2021-09-24 09:09:34 +0300

    Update lr2.c

--- 1sem/lr2.c
+++ 1sem/lr2.c
@@ -3,9 +3,11 @@
 
 int main()
 {
+    double b;
     double z1, z2, a;
     double pi=3.1415926;
     scanf("%lf", &a);
+    scanf("%lf", &b);
 
     if (a==45 || a==135) {
         printf("%s","Error, division by 0");
@@ -19,6 +21,10 @@ int main()
 
     printf("%.3f\n", z1);
     printf("%.3f\n", z2);
+
+    printf("%.2lf\n", sqrt(b));
+    printf("%.2lf\n",pow(b, b));
+    printf("%.2lf\n",cbrt(b));
 
     return 0;
 }
