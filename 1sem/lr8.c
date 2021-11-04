d3ae55dc4434963941eb170c8281c9b058695714
Dmitriy <89843200+quazzaar@users.noreply.github.com>
2021-11-04 23:37:23 +0300

    Update lr8.c

--- 1sem/lr8.c
+++ 1sem/lr8.c
@@ -4,17 +4,16 @@ int main(){
 
     char str1[100];
     char str2[100];
-    printf("First string: ");
-    gets(str1);
-    printf("Second string: ");
-    gets(str2);
-
+    printf("First string: \n");
+    scanf(" %[^\n]s", &str1);
+    printf("Second string: \n");
+    scanf(" %[^\n]s", &str2);
     char temp[100];
     strcpy(temp, str1);
-    printf("\n1) Concatenation: %s\n", strcat(temp, str2)); //сложение строк
+    printf("1) Concatenation: %s\n", strcat(temp, str2)); //сложение строк
 
     int n;
-    printf("\n4) Enter the number of characters you want to compare: "); //сравнение первых n символов
+    printf("4) Enter the number of characters you want to compare: "); //сравнение первых n символов
     scanf("%d",&n);
     printf("Comparison: ");
     if (strncmp(str1, str2, n)==0)
@@ -22,12 +21,12 @@ int main(){
     else
         printf("These symbols doesn't match\n");
 
-    printf("\n7)String length: %d\n", strlen(str1));  //длина строки
+    printf("7) String length: %d\n", strlen(str1));  //длина строки
 
     char key;
-    printf("\n10) Search symbol:");
+    printf("10) Search symbol:");
     scanf("%s", &key);
-    printf("\nYour symbol on %d position", strchr(str1,key) - str1 +1); //поиск символа, входящего во 2 строку в 1
+    printf("Your symbol on %d position", strchr(str1,key) - str1 +1); //поиск символа, входящего во 2 строку в 1
 
     printf("\n12) Number of different characters: %d\n", strcspn(str1, str2)); //
 
